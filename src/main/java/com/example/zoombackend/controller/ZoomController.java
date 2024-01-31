b2afac3434b1e6e39422a610d45dba9787eee151
Khachatur Yeritsyan <khachatur.yeritsyan@russianschool.com>
2024-01-31 19:14:21 +0400

    Resolve cross-origin resource sharing issue.

--- src/main/java/com/example/zoombackend/controller/ZoomController.java
+++ src/main/java/com/example/zoombackend/controller/ZoomController.java
@@ -13,6 +13,7 @@ public class ZoomController {
     private final ZoomService zoomService;
 
     @GetMapping("/connection-options")
+    @CrossOrigin
     public ConnectionOptions getConnectionOptions(@RequestParam String sessionName, @RequestParam String username) {
         return zoomService.getConnectionOptions(sessionName, username);
     }
