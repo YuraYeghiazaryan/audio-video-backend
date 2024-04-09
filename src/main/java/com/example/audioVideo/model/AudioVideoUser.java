424f9a3857970f8048a0a06537995ebdfc4fa657
Khachatur Yeritsyan <khachatur.yeritsyan@russianschool.com>
2024-04-09 20:32:13 +0400

    Fix AudioVideoUser isAudioOn and isVideoOn parameters serialization names.

--- src/main/java/com/example/audioVideo/model/AudioVideoUser.java
+++ src/main/java/com/example/audioVideo/model/AudioVideoUser.java
@@ -1,11 +1,14 @@
 package com.example.audioVideo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AudioVideoUser {
     private String id;
     private boolean joined;
+    @JsonProperty(value="isVideoOn")
     private boolean isVideoOn;
+    @JsonProperty(value="isAudioOn")
     private boolean isAudioOn;
 }
